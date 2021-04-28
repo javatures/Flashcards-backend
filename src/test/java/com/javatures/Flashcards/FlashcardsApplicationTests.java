35b2dea079de1204a083b6fb5f7e8fe2803d1993
Neruan-Revature <Neruan-Revature>
2021-04-28 12:27:45 -0700

    First commit with Spring quickstart

---
+++ src/test/java/com/javatures/Flashcards/FlashcardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatures.Flashcards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlashcardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
